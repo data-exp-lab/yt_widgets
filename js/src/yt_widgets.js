48688958395cff1f525c93020685f0e20cc6abf5
Nathanael Claussen <nclauss2@illinois.edu>
2017-07-26 11:44:13 -0500

    :ball:

--- js/src/yt_widgets.js
+++ js/src/yt_widgets.js
@@ -8,10 +8,10 @@ var _ = require('underscore');
 //
 // When serialiazing entire widget state for embedding, only values different from the
 // defaults will be specified.
-var InteractiveRenderModel = widgets.DOMWidgetModel.extend({
+var ViewModel = widgets.DOMWidgetModel.extend({
     defaults: _.extend(_.result(this, 'widgets.DOMWidgetModel.prototype.defaults'), {
-        _model_name : 'InteractiveRenderModel',
-        _view_name : 'InteractiveRenderView',
+        _model_name : 'ViewModel',
+        _view_name : 'ViewView',
         _model_module : 'yt-widgets',
         _view_module : 'yt-widgets',
         _model_module_version : '0.1.0',
@@ -19,63 +19,28 @@ var InteractiveRenderModel = widgets.DOMWidgetModel.extend({
     })
 });
 
-var util = yt.util;            // Not sure what these are called in YT
-var action = yt.action;        // Not sure what these are called in YT
-
-
-var seq = 1;
 // Custom View. Renders the widget model.
-var InteractiveRenderView = widgets.DOMWidgetView.extend({
-    render: function() {
-        this.url = this.model.get('url');
-        this.el.id = `InteractiveRender-${seq++}`;
-        this.req = {
-            plotId    : this.el.id,
-        };
-        this.model.on('change:zoom', this.update_zoom, this);
-        this.model.on('change:x_pan change:y_pan', this.update_pan, this);
-        this.redraw = this.redraw.bind(this);
-        this.update_zoom = this.update_zoom.bind(this);
-        this.zoom_changed = this.zoom_changed.bind(this);
-        this.update_pan = this.update_pan.bind(this);
-        this.pan_changed = this.pan_changed.bind(this);
-        setTimeout(this.redraw, 0);
-    },
+var ViewView = widgets.DOMWidgetView.extend({
+  render: function() {
 
-    redraw: function() {
-        if (this.hasOwnProperty("url") && (this.url.length === 0)) {
-            yt.InteractiveRender(this.el.id, this.req);
-        }
-        else {
-            console.log('using url ' + this.url);
-            firefly.InteractiveRender(this.el.id, {url: this.url, plotId: this.el.id});
-        }
-    }
+  },
 
-    update_zoom: function() {
-        console.log('updating zoom to ' + this.model.get('zoom'));
-        yt.zoom(this.model.get('zoom'));
-    },
+  redraw: function() {
 
-    zoom_changed: function(action,state) {        //callback for a zoom change
-        if (action.payload.plotId === this.req.plotId) {
-            var plot= yt.InteractiveRender();  // get the plot
-            console.log('I got a replot, zoom factor= ' + plot.zoomFactor);
-            zoom_factor = Math.round(parseFloat(plot.zoomFactor)*100)/100
-            var original_zoom = Math.round(this.model.get('zoom')*100)/100;
-            console.log('model zoom = ' + original_zoom);
-            if (zoom_factor != original_zoom){
-                console.log('updating model zoom to ' + zoom_factor);
-                this.model.set('zoom', zoom_factor);
-                this.touch();
-            }
-        }
-     },
+  },
+
+  update_zoom: function() {
+
+  },
+
+  update_pan: function() {
+
+  },
 
 });
 
 // BOILERPLATE:
 module.exports = {
-    ImageModel : InteractiveRenderModel,
-    ImageView : InteractiveRenderView
+    ImageModel : ViewModel,
+    ImageView : ViewView
 };
--- yt_widgets/yt_widgets.py
+++ yt_widgets/yt_widgets.py
@@ -2,18 +2,18 @@ import ipywidgets as widgets
 from traitlets import Unicode, Bool, Integer, Float, TraitError, validate
 
 
-@widgets.register('InteractiveRender')
-class InteractiveRender(widgets.DOMWidget):
+@widgets.register('View')
+class View(widgets.DOMWidget):
     """
-    InteractiveRender Widget
+    View Widget
     """
-    _model_name = Unicode('InteractiveRenderModel').tag(sync=True)
+    _model_name = Unicode('ViewModel').tag(sync=True)
     _model_module = Unicode('yt_widgets').tag(sync=True)
     _model_module_version = Unicode('0.1.0').tag(sync=True)
-    _view_name = Unicode('InteractiveRenderView').tag(sync=True)
+    _view_name = Unicode('ViewView').tag(sync=True)
     _view_module = Unicode('yt_widgets').tag(sync=True)
     _view_module_version = Unicode('0.1.0').tag(sync=True)
+
     x_pan = Float(1.0).tag(sync=True)
     y_pan = Float(1.0).tag(sync=True)
     zoom = Float(1.0).tag(sync=True)
-    url = Unicode('').tag(sync=True)
