9184d4cc628bdade7f90b45977a005e7b18f1e23
Nathanael Claussen <nclauss2@illinois.edu>
2017-08-04 17:17:05 -0500

    new

--- js/src/yt_widgets.js
+++ js/src/yt_widgets.js
@@ -32,8 +32,34 @@ var ViewView = widgets.DOMWidgetView.extend({
   parameters_changed: function() {
       this.el.textContent = this.model.get('parameters');
   }
-
 });
+
+var DisplayParamModel = widgets.DOMWidgetModel.extend({
+    defaults: _.extend(_.result(this, 'widgets.DOMWidgetModel.prototype.defaults'), {
+        _model_name : 'DisplayParamModel',
+        _view_name : 'DisplayParamView',
+        _model_module : 'yt_widgets',
+        _view_module : 'yt_widgets',
+        _model_module_version : '0.1.0',
+        _view_module_version : '0.1.0'
+    })
+});
+
+var DisplayParamView = widgets.DOMWidgetView.extend({
+  render: function() {
+      this.req = {
+        param : {}
+      }
+      this.param_changed():
+      this.model.on('change:param', this.param_changed, this);
+  },
+
+  param_changed: function() {
+      this.el.textContent = this.model.get('param');
+  }
+});
+
+
 
 // BOILERPLATE:
 module.exports = {
--- yt_widgets/yt_widgets.py
+++ yt_widgets/yt_widgets.py
@@ -22,23 +22,40 @@ class View(widgets.DOMWidget):
             print('AttributeError, needs fixing')
             raise
 
-@widgets.register('TextureSlider')
-class TextureSlider(widgets.FloatSlider):
+@widgets.register('DisplayParameters')
+class DisplayParam(widgets.DOMWidget):
     """
-    @todo:
-    Widget for slicing through Mesh objects.
+    Base class to a widget. Displays the selected parameter in this.el.textContent .
+    """
+    _model_name = Unicode('DisplayParam').tag(sync=True)
+    _model_module = Unicode('yt_widgets').tag(sync=True)
+    _model_module_version = Unicode('0.1.0').tag(sync=True)
+    _view_name = Unicode('DisplayParam').tag(sync=True)
+    _view_module = Unicode('yt_widgets').tag(sync=True)
+    _view_module_version = Unicode('0.1.0').tag(sync=True)
+    param = Dict().tag(sync=True)
+
+    def __init__(self, **kwargs):
+        """
+        The link is from the value attribute of the Dropdown widget/class
+        """
+
+@widgets.register('Dropdown')
+class Dropdown(DisplayParam):
+    """
+    Dropdown menu widget, derived from the DisplayParam class.
     """
 
-    def __init__(self, field, **kwargs):
-        """Extends FloatSlider from ipywidgets.
+    def __init__(self, ds, **kwargs):
+        """ Extends "Dropdown" class from ipywidgets
+
         Args:
-            field:    //see: @todo
-            **kwargs: Arbitrary keyword arguments for FloatRangeSlider.
+            ds: The dataset associated with this menu object.
+            kwargs: Arbitrary keyword arguments for "Dropdown" widget.
         """
-        super(TextureSlider, self).__init__(**kwargs)
-        self.min, self.max = (-10, 10)
-        self.value = 0
-        self.step = 0.1
+
+        super(Dropdown, self).__init__(**kwargs)
+        self._ds = ds
+        self.description = 'Parameters'
         self.layout.width = '100%'
-        self.description = "Slice Position"
-        self.orientation = 'vertical'
+        self.options = self._ds.parameters
