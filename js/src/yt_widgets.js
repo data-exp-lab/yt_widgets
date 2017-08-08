6a6dd338fe4ce9b668c3b68a96896f2dff402a26
Nathanael Claussen <nclauss2@illinois.edu>
2017-08-08 15:01:46 -0500

    :paperclip:

--- js/src/yt_widgets.js
+++ js/src/yt_widgets.js
@@ -8,10 +8,11 @@ var _ = require('underscore');
 //
 // When serialiazing entire widget state for embedding, only values different from the
 // defaults will be specified.
-var ViewModel = widgets.DOMWidgetModel.extend({
+
+var DisplayParamModel = widgets.DOMWidgetModel.extend({
     defaults: _.extend(_.result(this, 'widgets.DOMWidgetModel.prototype.defaults'), {
-        _model_name : 'ViewModel',
-        _view_name : 'ViewView',
+        _model_name : 'DisplayParamModel',
+        _view_name : 'DisplayParamView',
         _model_module : 'yt_widgets',
         _view_module : 'yt_widgets',
         _model_module_version : '0.1.0',
@@ -20,7 +21,7 @@ var ViewModel = widgets.DOMWidgetModel.extend({
 });
 
 // Custom View. Renders the widget model.
-var ViewView = widgets.DOMWidgetView.extend({
+var DisplayParamView = widgets.DOMWidgetView.extend({
   render: function() {
       this.req = {
         parameters : {}
@@ -34,35 +35,9 @@ var ViewView = widgets.DOMWidgetView.extend({
   }
 });
 
-var DisplayParamModel = widgets.DOMWidgetModel.extend({
-    defaults: _.extend(_.result(this, 'widgets.DOMWidgetModel.prototype.defaults'), {
-        _model_name : 'DisplayParamModel',
-        _view_name : 'DisplayParamView',
-        _model_module : 'yt_widgets',
-        _view_module : 'yt_widgets',
-        _model_module_version : '0.1.0',
-        _view_module_version : '0.1.0'
-    })
-});
-
-var DisplayParamView = widgets.DOMWidgetView.extend({
-  render: function() {
-      this.req = {
-        param : {}
-      }
-      this.param_changed():
-      this.model.on('change:param', this.param_changed, this);
-  },
-
-  param_changed: function() {
-      this.el.textContent = this.model.get('param');
-  }
-});
 
 
-
-// BOILERPLATE:
 module.exports = {
-    ViewModel : ViewModel,
-    ViewView : ViewView
+    DisplayParamModel : DisplayParamModel,
+    DisplayParamView : DisplayParamView
 };
--- yt_widgets/yt_widgets.py
+++ yt_widgets/yt_widgets.py
@@ -2,30 +2,11 @@ import ipywidgets as widgets
 from traitlets import Unicode, Dict, validate
 
 
-@widgets.register('View')
-class View(widgets.DOMWidget):
-    """
-    View Widget
-    """
-    _model_name = Unicode('ViewModel').tag(sync=True)
-    _model_module = Unicode('yt_widgets').tag(sync=True)
-    _model_module_version = Unicode('0.1.0').tag(sync=True)
-    _view_name = Unicode('ViewView').tag(sync=True)
-    _view_module = Unicode('yt_widgets').tag(sync=True)
-    _view_module_version = Unicode('0.1.0').tag(sync=True)
-    parameters = Dict({}).tag(sync=True)
-
-    def __init__(self, ds):
-        try:
-            self.parameters = ds.parameters
-        except AttributeError:
-            print('AttributeError, needs fixing')
-            raise
-
-@widgets.register('DisplayParameters')
+@widgets.register('DisplayParam')
 class DisplayParam(widgets.DOMWidget):
     """
-    Base class to a widget. Displays the selected parameter in this.el.textContent .
+    Base class to a (Dropdown) widget. Displays the selected parameter in this.el.textContent .
+    Self-updating (see yt_widget.js)
     """
     _model_name = Unicode('DisplayParam').tag(sync=True)
     _model_module = Unicode('yt_widgets').tag(sync=True)
@@ -33,12 +14,18 @@ class DisplayParam(widgets.DOMWidget):
     _view_name = Unicode('DisplayParam').tag(sync=True)
     _view_module = Unicode('yt_widgets').tag(sync=True)
     _view_module_version = Unicode('0.1.0').tag(sync=True)
-    param = Dict().tag(sync=True)
+    parameters = Dict().tag(sync=True)
+    w = widgets.Dropdown()
 
-    def __init__(self, **kwargs):
+    def __init__(self, ds):
         """
-        The link is from the value attribute of the Dropdown widget/class
+        The link should be from the value attribute of the Dropdown widget/class
         """
+        try:
+            self.parameters = ds.parameters
+        except AttributeError:
+            print('AttributeError, needs fixing')
+            raise
 
 @widgets.register('Dropdown')
 class Dropdown(DisplayParam):
@@ -47,15 +34,7 @@ class Dropdown(DisplayParam):
     """
 
     def __init__(self, ds, **kwargs):
-        """ Extends "Dropdown" class from ipywidgets
-
-        Args:
-            ds: The dataset associated with this menu object.
-            kwargs: Arbitrary keyword arguments for "Dropdown" widget.
+        """Extends "DisplayParam" class from yt_widgets
+           Extends "Dropdown" class from ipywidgets
         """
-
-        super(Dropdown, self).__init__(**kwargs)
-        self._ds = ds
-        self.description = 'Parameters'
-        self.layout.width = '100%'
-        self.options = self._ds.parameters
+        super(DisplayParam, self).__init__(**kwargs)
