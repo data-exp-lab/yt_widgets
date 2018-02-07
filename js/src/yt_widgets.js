0a60291323d7fa9298eefb75c4aa45b2bc740cdd
Nathanael Claussen <nclauss2@illinois.edu>
2018-02-06 19:26:48 -0600

    fixes involving upgraded three support

--- js/src/yt_widgets.js
+++ js/src/yt_widgets.js
@@ -89,7 +89,12 @@ var HelloWorldView = widgets.DOMWidgetView.extend({
 //      var geometry, material, mesh;
 
       render: function() {
-        this.el.append(this.init());
+        var scene = new THREE.Scene();
+        var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+        var renderer = new THREE.WebGLRenderer();
+        renderer.setSize( window.innerWidth, window.innerHeight );
+        document.body.appendChild( renderer.domElement );
       },
 
       init: function() {
